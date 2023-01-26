546755c902e2abcffdecf95769a05e614605e22e
Chris-C3A <anon_c3a@outlook.com>
2023-01-26 19:52:33 +0000

    boilerplate gofiber app

--- server.go
+++ server.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+var HOST string = "0.0.0.0"
+var PORT string = "3000"
 
 func main() {
-    fmt.Println("Hello")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	// fmt.Printf("Server running at %s:%s", HOST, PORT)
+	app.Listen(HOST + ":" + PORT)
 }
